560c31a5305266e16f8fa8977a738f37637e610c
dominusmars <khawammohamad99@gmail.com>
2025-02-18 11:30:12 -0500

    removed dist

--- dist/utils/log.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function log(message, level = "info") {
-    const timestamp = new Date().toISOString();
-    console.log(`[${timestamp}] [${level.toUpperCase()}]: ${message}`);
-}
-exports.default = log;
